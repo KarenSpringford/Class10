46a75f1596345be45de2fc1c2af06eba22a40b00
KarenSpringford <jkspringford@gmail.com>
2016-07-27 09:18:28 -0400

    Initial Commit

---
+++ Class10/Models/Album.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Class10.Models
+{
+    public class Album
+    {
+        /// <summary>
+        /// This is the empty constuctor
+        /// </summary>
+        /// 
+        public Album()
+        {
+
+        }
+
+        /// <summary>
+        /// This constructor takes one parameter - Title
+        /// </summary>
+        /// 
+
+            public Album (string Title)
+        {
+            this.Title = Title;
+        }
+
+        public int AlbumID { get; set; }
+
+
+        public string Title { get; set; }
+
+        public Genre Genre { get; set; }
+    }
+}
---
+++ Class10/Models/Genre.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Class10.Models
+{
+    public class Genre
+    {
+        /// <summary>
+        /// This is the empty constructor
+        /// </summary>
+        /// 
+        public Genre()
+        {
+
+        }
+
+        /// <summary>
+        /// This consturcotr takes one parameter - Name
+        /// </summary>
+        /// 
+        public Genre (string Name)
+        {
+            this.Name = Name;
+        }
+
+        public int GenreID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
