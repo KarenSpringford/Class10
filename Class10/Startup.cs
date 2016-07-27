32b3f053c80eb41d55a4f209b1fd2b6190bf838c
KarenSpringford <jkspringford@gmail.com>
2016-07-27 08:58:43 -0400

    Add project files.

---
+++ Class10/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Class10.Startup))]
+namespace Class10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
