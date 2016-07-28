cbf26efb2586915d549540348d4759e21f384450
KarenSpringford <jkspringford@gmail.com>
2016-07-27 22:49:10 -0400

    Added Data Annotations for some Validation and Display Enhancements. This closes Issue #13

--- Class10/Models/Album.cs
+++ Class10/Models/Album.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Class10.Models
 {
+    [Bind(Exclude = "AlbumId")]
+
     public class Album
     {
         /// <summary>
@@ -25,12 +30,26 @@ namespace Class10.Models
         {
             this.Title = Title;
         }
+
+        [ScaffoldColumn(false)]
         public virtual int AlbumId { get; set; }
+
+        [DisplayName("Genre")]
         public virtual int GenreId { get; set; }
+
+        [DisplayName("Artist")]
         public virtual int ArtistId { get; set; }
+
+
         public virtual string Title { get; set; }
+
         public virtual decimal Price { get; set; }
+
+        [DisplayName("Album Art URL")]
+ 
         public virtual string AlbumArtUrl { get; set; }
+
+
         public virtual Genre Genre { get; set; }
         public virtual Artist Artist { get; set; }
     }
