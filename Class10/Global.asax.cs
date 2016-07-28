9a2b2d029ab7664c1ab631fd91e5d15c479a06b8
KarenSpringford <jkspringford@gmail.com>
2016-07-27 20:15:26 -0400

    Updated ConnectionStrings Added Context Class and Added Sample Data.  Closes Issue #4,  Issue #5, and Issue #6

--- Class10/Global.asax.cs
+++ Class10/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Class10.Models;
 
 namespace Class10
 {
@@ -12,6 +13,7 @@ namespace Class10
     {
         protected void Application_Start()
         {
+            System.Data.Entity.Database.SetInitializer(new SampleData());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
--- Class10/Models/Artist.cs
+++ Class10/Models/Artist.cs
@@ -7,7 +7,7 @@ namespace Class10.Models
 {
     public class Artist
     {
-        public int ArtistId { get; set; }
-        public string Name { get; set; }
+        public virtual int ArtistId { get; set; }
+        public virtual string Name { get; set; }
     }
 }
